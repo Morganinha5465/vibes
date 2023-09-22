675529a1b66c218cd6af0dbbc42288098f550811
Morganinha5465 <143187440+Morganinha5465@users.noreply.github.com>
2023-09-22 09:06:44 -0300

    add arquivos

---
+++ sketch.js
@@ -0,0 +1,24 @@
+function setup() {
+    createCanvas(900, 600);
+     background("purple");
+  }
+  
+  function draw() {
+  
+    stroke("red");
+    
+    fill("rgb(129,0,0)");
+    
+    if(mouseIsPressed) {
+  
+    
+    rect(mouseX, mouseY, 50, 50); 
+    
+    
+    
+  
+    
+  } 
+    
+  
+  }
